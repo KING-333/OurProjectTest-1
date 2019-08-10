044b2d1a708546e37ca81c4fb2f4f7f21ac3ec7d
KING-333 <3370452728@qq.com>
2019-08-10 23:15:43 +0800

    修改

--- app/src/main/java/com/example/ourprojecttest/MainActivity.java
+++ app/src/main/java/com/example/ourprojecttest/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     //我是魔鬼1111111111111
     @Override
-    111111111
+    1111111111
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
